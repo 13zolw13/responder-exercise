779277d2cb34ffce37b97147ba350c5001db98fc
tomek <tomaszpawelkowalski@gmail.com>
2022-05-31 23:11:30 +0200

    test: create test for question details endpoint

--- fpwd/tests/questions.e2e.test.js
+++ fpwd/tests/questions.e2e.test.js
@@ -8,10 +8,15 @@ describe('e2e tests', () => {
     expect(response.statusCode).toBe(200)
   })
 
-      test(' #/questions should return questions', async () => {
-        const response = await request(app).get('/questions ')
-        expect(response.statusCode).toBe(200)
-        expect(response.body).toHaveLength(2)
-      })
-    
+  test(' #/questions should return questions', async () => {
+    const response = await request(app).get('/questions ')
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveLength(2)
+  })
+
+  test(' #/questions/:questionId should return question details', async () => {
+    const questionId = '0f9e662-fa0e-4ec7-b53b-7845e8f821c3'
+    const response = await request(app).get(`/questions/${questionId}`)
+    expect(response.statusCode).toBe(200)
+  })
 })
