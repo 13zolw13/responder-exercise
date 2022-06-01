408858f77250ab4a48603b563c6c129f9453d1df
tomek <tomaszpawelkowalski@gmail.com>
2022-06-01 10:43:38 +0200

    test(e2e): create test for add new question

--- fpwd/tests/questions.e2e.test.js
+++ fpwd/tests/questions.e2e.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 const app = require('../index')
-
+const QuestionsMock = require('./QuestionsMock')
 describe('e2e tests', () => {
   test(' #/ should  return message ', async () => {
     const response = await request(app).get('/')
@@ -11,12 +11,18 @@ describe('e2e tests', () => {
   test(' #/questions should return questions', async () => {
     const response = await request(app).get('/questions ')
     expect(response.statusCode).toBe(200)
-    expect(response.body).toHaveLength(2)
   })
 
   test(' #/questions/:questionId should return question details', async () => {
     const questionId = '0f9e662-fa0e-4ec7-b53b-7845e8f821c3'
     const response = await request(app).get(`/questions/${questionId}`)
     expect(response.statusCode).toBe(200)
+  })
+
+  test('should add new question', async () => {
+    const response = await request(app)
+      .post('/questions')
+        .send(QuestionsMock[1])
+    expect(response.statusCode).toBe(201)
   })
 })
